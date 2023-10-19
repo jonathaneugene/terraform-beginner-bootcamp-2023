b94bc4fd3a3239b1a075deef9d41d09b27f2fe5b
jonathan eugene <jeugene@buffalo.edu>
2023-10-19 18:34:36 +0000

    #42 wip setting up the skeleton for creating a custom terraform provider

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -1,32 +1,36 @@
 // package main: Declares the package name. 
 // The main package is special in Go, it's where the execution of the program starts.
-
 package main
 
-import {
+// fmt is short format, it contains functions for formatted I/O.
+import (
+	// "log"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-}
-
+)
+// func main(): Defines the main function, the entry point of the app. 
+// When you run the program, it starts executing from this function.
 func main() {
-
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider
+		ProviderFunc: Provider,
 	})
-	fmt.Println("Hello World!")
+	// Format.PrintLine
+	// Prints to standard output
+	fmt.Println("Hello, world!")
 }
 
+// in golang, a titlecase function will get exported.
 func Provider() *schema.Provider {
 	var p *schema.Provider
 	p = &schema.Provider{
 		ResourcesMap:  map[string]*schema.Resource{
-			"terratowns_home": Resource(),
+
 		},
 		DataSourcesMap:  map[string]*schema.Resource{
 
 		},
-		Schema:  map[string]*schema.Schema{
+		Schema: map[string]*schema.Schema{
 			"endpoint": {
 				Type: schema.TypeString,
 				Required: true,
@@ -42,10 +46,19 @@ func Provider() *schema.Provider {
 				Type: schema.TypeString,
 				Required: true,
 				Description: "UUID for configuration",
-				ValidateFunc: validateUUID,
+				//ValidateFunc: validateUUID,
 			},
-		}
+		},
 	}
-	p.ConfigureContextFunc = providerConfigure(p)
+	//p.ConfigureContextFunc = providerConfigure(p)
 	return p
 }
+
+//func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+//	log.Print('validateUUID:start')
+//	value := v.(string)
+//	if _,err = uuid.Parse(value); err != nil {
+//		errors = append(error, fmt.Errorf("invalid UUID format"))
+//	}
+//	log.Print('validateUUID:end')
+//}
